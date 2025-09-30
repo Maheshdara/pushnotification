b93fbed487ed9be86a04f11c863d77f15daf268d
Maheshdara <88979927+Maheshdara@users.noreply.github.com>
2025-09-30 13:47:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const admin = require("firebase-admin");
@@ -46,6 +47,7 @@ app.post("/send-notification", async (req, res) => {
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+
 
 
 
